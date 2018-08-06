f848e7c0e484025e6ced769d257513732511f8df
boterosalas <boterosalas@gmail.com>
2018-08-05 23:09:12 -0500

    cambio de id's por id's sin guiones medios. Utilizar mejor guiones bajos o uppercase

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 var button = document.querySelector('.questionnaire');
-var modal = document.getElementById('modal-dad');
-var modal_ch = document.getElementById('modal-ch');
-var cierre_modal = document.getElementById('cierre-modal');
-var boton_menu_mobile = document.getElementById('boton-menu-mobile');
-var menu_mobile = document.getElementById('menu-mobile');
+var modal = document.getElementById('modal_dad_formulario');
+var modal_ch = document.getElementById('modal_ch');
+var cierre_modal = document.getElementById('cierreModal');
+var boton_menu_mobile = document.getElementById('boton_menu_mobile');
+var menu_mobile = document.getElementById('menuMobile');
 button.addEventListener("click", function(e){
     console.log(e.target);
-    if(e.target.getAttribute('id') == 'maintenance-button'){
-        modal.classList.add('modal-dad--active');
-        modal_ch.classList.add('modal-ch--active');
-    }
-    else if(e.target.getAttribute('id') == 'design-button'){
-        modal.classList.add('modal-dad--active');
+    if(e.target.getAttribute('id') == 'cuentanos'){
+        modal.classList.add('modal-dad-formulario--active');
         modal_ch.classList.add('modal-ch--active');
     }
 })
 modal.addEventListener("click", function(e){
-    if(e.target.getAttribute('id') == 'cierre-modal'){
+    if(e.target.getAttribute('id') == 'cierreModal'){
         console.log(e.target);
-        setTimeout(function(){modal.classList.remove('modal-dad--active');},200);
-        //modal.classList.remove('modal-dad--active');
+        setTimeout(function(){modal.classList.remove('modal-dad-formulario--active');},200);
         modal_ch.classList.remove('modal-ch--active');
     }
 })
