5fda074e31a8b1190a5eeae396a5716afbaa2484
boterosalas <boterosalas@gmail.com>
2018-08-15 07:38:06 -0500

    begin the galery js

--- services.js
+++ services.js
@@ -4,16 +4,34 @@ window.addEventListener('load',function(){
     var galeria_diseno = document.querySelectorAll('#diseno-dad .galeria-mini');
     var galeria_jardineria = document.querySelectorAll('#jardineria-dad .galeria-mini');
     var foto_principal_mantenimiento = document.querySelector('#mantenimiento-dad .foto-principal-ch');
-    var foto_principal_diseno = document.querySelectorAll('#mantenimiento-dad .foto-principal-ch');
-    var foto_principal_jardineria = document.querySelectorAll('#mantenimiento-dad .foto-principal-ch');
-    
+    var foto_principal_diseno = document.querySelector('#diseno-dad .foto-principal-ch');
+    var foto_principal_jardineria = document.querySelector('#jardineria-dad .foto-principal-ch');
+
+    function descripcion(){
+        var btn = document.createElement("BUTTON");
+        var t = document.createTextNode("CLICK ME");
+        btn.appendChild(t);
+    }
+
     for(let i = 0; i < galeria_mantenimiento.length; i++){
         galeria_mantenimiento[i].addEventListener('click',function(){
             let color = this.style.background;
             foto_principal_mantenimiento.style.background = color;
         })
     }
-    
+    for(let i = 0; i < galeria_diseno.length; i++){
+        galeria_diseno[i].addEventListener('click',function(){
+            let color = this.style.background;
+            foto_principal_diseno.style.background = color;
+        })
+    }
+    for(let i = 0; i < galeria_jardineria.length; i++){
+        galeria_jardineria[i].addEventListener('click',function(){
+            let color = this.style.background;
+            foto_principal_jardineria.style.background = color;
+        })
+    }
+
     function animacion_scroll(y){
         var comienzo = setInterval(frame, 5);
         function frame() {
